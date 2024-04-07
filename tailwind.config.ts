734c3d0faaddaccd74ad9e892a8be4ea23d85d51
Pranav <pranavshelke4@gmail.com>
2024-04-08 01:25:23 +0530

    Fix box shadow color in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
       },
       boxShadow: {
         custom:
-          "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.6) 0px 8px 16px -8px;",
+          "rgba(0, 0, 0, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.6) 0px 8px 16px -8px;",
       },
     },
   },
