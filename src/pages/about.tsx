820de18e06fe7091c73e02f77448e1431f82cef6
pranav123451 <pranavjadhav432@gmail.com>
2024-04-08 00:31:19 +0530

    "Added about page"

---
+++ src/pages/about.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+const About = () => {
+  return (
+    <div className='main'>
+    <div className="card-container">
+<ul id="cards">
+  <li className="card" id="card1">
+    <div className="about_heading"><p className="about_heading">About Us</p></div>
+    <div className="card-body">
+      <h2 className="ted1">What is <span>Ted</span> ?</h2>
+      <p className="ted1para">
+        TED is a non-profit organization devoted to Ideas Worth Spreading.
+        It started as a four-day conference in California 39 years ago,
+        believing passionately in the power of ideas to change attitudes
+        and lives and welcoming people from every discipline and culture
+        who seek a deeper understanding of the world. Today, the two
+        annual TED Conferences invite the world's leading thinkers and
+        doers to speak for 18 minutes or less, and the total number of
+        events conducted till now is more than 6000. All this has led to a
+        global community focused on change through the power of ideas,
+        thus forming a trinity of ideas from- Technology, Entertainment,
+        and Design, and has recently taken up a broader scope ranging from
+        science and business to global issues.
+      </p>
+    </div>
+  </li>
+  <li className="card" id="card2">
+    <div className="card-body">
+      <h2 className="ted1">What is <span>Tedx</span> ?</h2>
+      <p className="ted1para">
+        In the spirit of ideas worth spreading, TEDx is a TED-created
+        program of local, self-organized events that bring people together
+        to share a TED-like experience. These events are community driven
+        and are organized by passionate individuals who seek to uncover
+        new ideas and share the latest research in their field of
+        expertise that spark conversations in their communities and
+        ultimately elevate them to a global stage. TEDx events aim to
+        integrate ideas from every nook and corner of the world, thus,
+        extending the boundaries of views and ideas. Our event is named
+        TEDxVITPune, where x = independently organized TED event. The TED
+        Conference provides general guidance for the TEDx program, but
+        individual TEDx events, including ours, are self-organized.
+      </p>
+    </div>
+  </li>
+  <li className="card" id="card3">
+    <div className="card-body">
+      <h2 className="ted1">What is <span>Tedx</span>VITPune?</h2>
+      <p className="ted1para">
+        TEDxVITPune was among the first TEDx events in Pune and has grown
+        to be a premier platform for furthering TED's mission of 'Ideas
+        Worth Spreading' within its local community. Over the past nine
+        events, TEDxVITPune has brought together visionaries, thinkers,
+        and doers of different disciplines from around the country to
+        create a one-day event filled with ideas and discussions that
+        contribute to meaningful change. The event has hosted the likes of
+        Amira Gill, Avinash Ramanathan, Mahesh Kale, Debanshu Roy, Jishnu
+        Chatterjee, Mark Hannant, Ajit Kembhavi, Avinash Dharmadhikari,
+        Niranjan Pedanekar, Chintan Ruparel, Kunal Jagwani, and Swanandi
+        Tikekar. Cumulatively, the event has garnered an extensive
+        following around the country and has been recognized as an
+        exemplary TEDx event in India.
+      </p>
+    </div>
+  </li>
+</ul>
+</div>
+</div>
+  );
+};
+
+export default About;
