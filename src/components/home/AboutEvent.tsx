65794426789764264bc5691f335546b7456a2fbb
pranav123451 <pranavjadhav432@gmail.com>
2024-04-08 14:33:14 +0530

    Update AboutEvent component with event details

--- src/components/home/AboutEvent.tsx
+++ src/components/home/AboutEvent.tsx
@@ -4,7 +4,23 @@ import React from "react";
 const AboutEvent = () => {
   return (
     <div className="about-event">
-      <h1>About Event</h1>
+      <div className="div1">
+        <h5>27th April, 2024</h5>
+        <h1>The Space Between</h1>
+      </div>
+      <div className="div2">
+        We fearlessly embark on a journey of discovery, navigating through the
+        unfamiliar paths of a complex problem. Our thirst for knowledge leads us
+        to constantly seek fresh insights and perspectives that will steer us
+        towards the right path. By immersing ourselves in the diverse realities
+        we encounter, we strive to reveal the concealed truths that will guide
+        us to a place of genuine tranquility and inner peace, even amid chaos
+        and uncertainty. The world is brimming with fragments of inspiration,
+        ideas, and emotions. Our unique experiences shape us into the
+        individuals we are today, each one remarkable in its own way. All we
+        need to do is unearth and piece together these fragments, unlocking our
+        full potential.
+      </div>
     </div>
   );
 };
