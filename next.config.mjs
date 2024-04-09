32a8e4fb402502841e8b75400659795685e0878e
Tilak Dave <davetilak003@gmail.com>
2024-04-09 14:22:02 +0530

    redirect links & time line update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/speaker-nomination',
+        destination: 'https://forms.gle/WWS1JkCdwN5LTinc8',
+        permanent: true,
+      },
+      {
+        source: '/tickets-vit',
+        destination: 'https://learner.vierp.in',
+        permanent: true,
+      },
+      {
+        source: '/tickets-non-vit',
+        destination: 'https://learner.vierp.in/events',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
