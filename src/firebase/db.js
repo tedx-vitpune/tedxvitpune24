9fca5b16e53d7670d8c89c446171252a91fde595
pranav123451 <pranavjadhav432@gmail.com>
2024-12-06 18:09:54 +0530

    keys updated env file

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA61oET7Z6ZnUt_eFyPtIvRjexb8iQZORM",
-    authDomain: "tedx-pune.firebaseapp.com",
-    projectId: "tedx-pune",
-    storageBucket: "tedx-pune.firebasestorage.app",
-    messagingSenderId: "413730516955",
-    appId: "1:413730516955:web:2b26d0ba0825bab017c1fe",
-    measurementId: "G-VR47265WL4"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
